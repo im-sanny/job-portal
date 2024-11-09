b83d545617c8a19142042ed96b55398dde265c88
im-sanny <raselparvezsanny@gmail.com>
2024-11-09 22:56:32 +0600

    add: email password sign in method added

--- src/pages/Authentication/Login.jsx
+++ src/pages/Authentication/Login.jsx
@@ -22,6 +22,23 @@ const Login = () => {
   };
 
   // email pass signIn
+  const handleSignIn = async (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const email = form.email.value;
+    const pass = form.password.value;
+    console.log({ email, pass });
+    try {
+      // user login
+      const result = await signIn(email, pass);
+      console.log(result);
+      navigate("/");
+      toast.success("Sign In Successful");
+    } catch (error) {
+      console.log(error);
+      toast.error(error?.message);
+    }
+  };
 
   return (
     <div className="flex justify-center items-center min-h-[calc(100vh-306px)]">
@@ -42,7 +59,10 @@ const Login = () => {
             Welcome back!
           </p>
 
-          <div onClick={handleGoogleSignIn} className="flex cursor-pointer items-center justify-center mt-4 text-gray-600 transition-colors duration-300 transform border rounded-lg   hover:bg-gray-50 ">
+          <div
+            onClick={handleGoogleSignIn}
+            className="flex cursor-pointer items-center justify-center mt-4 text-gray-600 transition-colors duration-300 transform border rounded-lg   hover:bg-gray-50 "
+          >
             <div className="px-4 py-2">
               <svg className="w-6 h-6" viewBox="0 0 40 40">
                 <path
@@ -78,7 +98,7 @@ const Login = () => {
 
             <span className="w-1/5 border-b dark:border-gray-400 lg:w-1/4"></span>
           </div>
-          <form>
+          <form onClick={handleSignIn}>
             <div className="mt-4">
               <label
                 className="block mb-2 text-sm font-medium text-gray-600 "
