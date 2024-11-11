f801a97a122b920e349d05275e92003e5916b79d
im-sanny <raselparvezsanny@gmail.com>
2024-11-11 06:30:29 +0600

    carousel component added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
+import Carousel from "../components/Carousel";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div>
+      <Carousel />
+    </div>
+  );
+};
 
-export default Home
+export default Home;
