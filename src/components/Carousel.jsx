c8ceaeaf18826a374b0a104b0cc401defc969740
im-sanny <raselparvezsanny@gmail.com>
2024-11-11 07:29:26 +0600

    add: slide component add

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,20 +1,21 @@
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/pagination';
-import 'swiper/css/navigation';
-
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
 
 // import required modules
-import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+import { Autoplay, Pagination, Navigation } from "swiper/modules";
+import Slide from "./Slide";
 
 export default function Carousel() {
   return (
-    <>
+    <div className="">
       <Swiper
         spaceBetween={30}
         centeredSlides={true}
+        loop={true}
         autoplay={{
           delay: 2500,
           disableOnInteraction: false,
@@ -26,16 +27,16 @@ export default function Carousel() {
         modules={[Autoplay, Pagination, Navigation]}
         className="mySwiper"
       >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
+        <SwiperSlide>
+          <Slide></Slide>
+        </SwiperSlide>
+        <SwiperSlide>
+          <Slide></Slide>
+        </SwiperSlide>
+        <SwiperSlide>
+          <Slide></Slide>
+        </SwiperSlide>
       </Swiper>
-    </>
+    </div>
   );
 }
